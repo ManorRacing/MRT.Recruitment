37d28a1cc6a879b489aeb70a8c281a3f01c9b71b
Steve O'Hara <steve.ohara@manorracing.com>
2016-10-10 10:31:01 +0100

    Added a controller for accessing Telemetry data using a typed repository. API methods can be accessed from http://localhost:5000/api/{methodname}

---
+++ src/Manor.DreamTeam.Recruitment/Controllers/TelemetryController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using Manor.DreamTeam.Recruitment.Domain;
+using Manor.DreamTeam.Recruitment.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Manor.DreamTeam.Recruitment.Controllers
+{
+    [Route("api/[controller]")]
+    public class TelemetryController : Controller
+    {
+        private readonly IRepository<Telemetry> _telemetryRepo;
+
+        public TelemetryController(IRepository<Telemetry> telemetryRepo)
+        {
+            _telemetryRepo = telemetryRepo;
+        }
+
+        // GET: api/telemetry
+        [HttpGet]
+        public IEnumerable<Telemetry> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        // GET api/telemetry/5
+        [HttpGet("{id}")]
+        public Telemetry Get(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
