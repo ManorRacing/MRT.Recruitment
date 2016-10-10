098a3a9140c09da58b9d8bc652e817966cc63cbd
Steve O'Hara <steve.ohara@manorracing.com>
2016-10-10 10:28:30 +0100

    Added repository interface

---
+++ src/Manor.DreamTeam.Recruitment/Interfaces/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace Manor.DreamTeam.Recruitment.Interfaces
+{
+    public interface IRepository<T>
+    {
+        IQueryable<T> Get();
+        T GetById(IComparable id);
+        void Create(T entity);
+        void Update(IComparable id, T entity);
+        void Delete(IComparable id);
+    }
+}
