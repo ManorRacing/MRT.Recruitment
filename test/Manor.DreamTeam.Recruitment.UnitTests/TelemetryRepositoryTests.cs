24059101f23cdead0ff377a093d075a37a80c892
Steve O'Hara <steve.ohara@manorracing.com>
2016-10-10 10:31:29 +0100

    Added unit test project

---
+++ test/Manor.DreamTeam.Recruitment.UnitTests/TelemetryRepositoryTests.cs
@@ -0,0 +1,15 @@
+﻿using Manor.DreamTeam.Recruitment.Domain;
+using Manor.DreamTeam.Recruitment.Interfaces;
+
+namespace Manor.DreamTeam.Recruitment.UnitTests
+{
+    public class TelemetryRepositoryTests
+    {
+        private IRepository<Telemetry> _repo;
+
+        public TelemetryRepositoryTests()
+        {
+            // TODO: Write some unit tests :)
+        }
+    }
+}
