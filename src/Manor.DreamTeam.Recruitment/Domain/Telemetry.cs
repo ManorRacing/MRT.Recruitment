b937a8d91687c3cf7c0db2e8a79ef4789e1523cb
Steve O'Hara <steve.ohara@manorracing.com>
2016-10-26 14:16:56 +0100

    Added IComparable interface to Telemetry class for telemetry sorting. Candidates can now implement any form of ID, as long as it derives from IComparable.

--- src/Manor.DreamTeam.Recruitment/Domain/Telemetry.cs
+++ src/Manor.DreamTeam.Recruitment/Domain/Telemetry.cs
@@ -2,11 +2,16 @@
 
 namespace Manor.DreamTeam.Recruitment.Domain
 {
-    public class Telemetry
+    public class Telemetry : IComparable<Telemetry>
     {
-        public IComparable<Guid> Id { get; set; }
+        public IComparable Id { get; set; }
         public DateTime TimeStamp { get; set; }
         public Lap Lap { get; set; }
         public Car Car { get; set; }
+
+        public int CompareTo(Telemetry other)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
